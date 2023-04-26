0d8f71dc28864eb32547632ee4688e927de7d8a6
Tarabass <p.rietveld@live.com>
2023-04-26 15:11:43 +0200

    Add Mui CssBaseline

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
+			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+			<CssBaseline />
 			<App />
 		</ThemeProvider>
 	</React.StrictMode>
