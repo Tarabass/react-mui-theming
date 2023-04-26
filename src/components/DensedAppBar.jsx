9279003e6ca89f90c78f292a7daa3a02be17f6ea
Tarabass <p.rietveld@live.com>
2023-04-26 15:33:53 +0200

    Replace menu icon with Catmandu logo

--- src/components/DensedAppBar.jsx
+++ src/components/DensedAppBar.jsx
@@ -1,18 +1,26 @@
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
+import { AppBar, Toolbar, Typography } from '@mui/material'
 
 function DensedAppBar() {
 	return (
 		<AppBar position="static">
 			<Toolbar variant="dense">
-				<IconButton
-					edge="start"
-					color="inherit"
-					aria-label="menu"
-					sx={{ mr: 2 }}
+				<Typography
+					variant="h6"
+					noWrap
+					component="a"
+					href="/"
+					sx={{
+						mr: 2,
+						display: { xs: 'none', md: 'flex' },
+						fontFamily: 'monospace',
+						fontWeight: 700,
+						letterSpacing: '.3rem',
+						color: 'inherit',
+						textDecoration: 'none',
+					}}
 				>
-					<MenuIcon />
-				</IconButton>
+					Catmandu
+				</Typography>
 				<Typography variant="h6" color="inherit" component="div">
 					Photos
 				</Typography>
